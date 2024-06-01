65e03eca537c03d8d8f47976f1e3611e67b85c86
Zanotto-Enrico <887566@stud.unive.it>
2024-06-01 11:15:07 +0200

    downloaded records changed from A ro R

--- downloader.py
+++ downloader.py
@@ -13,38 +13,42 @@ AS_TO_DISCOVER = 10000
 #############################################################
 SAVE_FILE = 'bgp_paths.pkl'
 
-
-
 def get_bgp_data():
     stream = pybgpstream.BGPStream(
         from_time="2024-05-01 00:00:00",  
         until_time="2024-05-01 23:59:59",
         collectors=["route-views.wide"],
-        record_type="ribs",
-        filter="type updates"
+        record_type="ribs"  # Using RIB records for a full routing table snapshot
     )
 
-    print("finding AS numbers and paths ...")
+    print("Finding AS numbers and paths ...")
 
     progress_bar = tqdm(total=AS_TO_DISCOVER)
     bgp_data = set()
+    asn_found = set()
     counter = 0
+
     for rec in stream:
-        # TODO FIX: we are not counting the AS found but the links !!! 
-        if  len(bgp_data) > 10000: break
+        if len(asn_found) >= AS_TO_DISCOVER:
+            break
         for elem in rec.record:
-            if elem.type == "A":  # BGP Announcement
+            if elem.type == "R":  # RIB record
+                print(elem)
                 as_path = elem.fields["as-path"].split(" ")
                 for i in range(len(as_path) - 1):
                     src_as = as_path[i]
                     dst_as = as_path[i + 1]
+                    print(src_as, dst_as)
                     bgp_data.add((src_as, dst_as))
-                    progress_bar.update(len(bgp_data) - counter)
-                    counter = len(bgp_data)
+                    if src_as not in asn_found:
+                        asn_found.add(src_as)
+                        progress_bar.update(1)
+                    if dst_as not in asn_found:
+                        asn_found.add(dst_as)
+                        progress_bar.update(1)
+                    counter = len(asn_found)
+    progress_bar.close()
     return bgp_data
-
-
-
 
 # Main
 
@@ -53,4 +57,4 @@ bgp_data = get_bgp_data()
 print(f"Saving data...")
 with open(SAVE_FILE, 'wb') as f:
     pickle.dump(bgp_data, f)
-print(f"Data saved in  {SAVE_FILE}.")
+print(f"Data saved in {SAVE_FILE}.")
